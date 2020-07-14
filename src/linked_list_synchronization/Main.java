1e0d4e3262127c73381bde8574d495270b4df234
Noé Fernandes <noepessoa@outlook.com>
2020-07-14 14:07:35 -0300

    Criando classes

---
+++ src/linked_list_synchronization/Main.java
@@ -0,0 +1,10 @@
+package linked_list_synchronization;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ola mundo");
+	}
+
+}
